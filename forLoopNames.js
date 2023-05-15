bce2d7e634517cc441793f31510f8b4e50b68df2
Kieth2 <133469973+Kieth2@users.noreply.github.com>
2023-05-15 15:52:06 +0800

    Add files via upload

---
+++ forLoopNames.js
@@ -0,0 +1,14 @@
+const names = ['Christian', 'Bianca', 'Joven', 'Dominic', 'Jim', 'Lawrence', 'Rosemarie', 'Royce', 'Abelle', 'Caezzy', 'Edward', 'Ian', 'Jei', 
+'Jerome', 'John']
+
+for (let Name = 0;  names.length; Name++){
+
+    if (Name > names.length){
+        break;
+    }
+
+console.log(names[Name]) 
+
+}
+   
+ 
